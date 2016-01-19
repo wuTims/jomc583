0fc6deb1005463a54ad269e2b7c70a64129ba050
Tim Wu <tim_wu@live.com>
2016-01-19 10:36:49 -0500

    Problem 1 edit

--- class03/exercise01/exercise01.js
+++ class03/exercise01/exercise01.js
@@ -1,5 +1,17 @@
 //Problem 1
 function farToCel(farTemp){
-	var celTemp = (5/9) * (farTemp - 32)
-	console.log(parseInt(celTemp));
+	var celTemp = (5/9) * (farTemp - 32);
+    var tempAnalysis;
+    if(celTemp < 10){
+      tempAnalysis = "cold";
+    }else if(celTemp >= 10 && celTemp < 25){
+      tempAnalysis = "fair";
+    }else{
+      tempAnalysis = "hot";
+    }
+	console.log("The current temperature is " + parseInt(celTemp) + " degrees Celsius. It's " + tempAnalysis + " outside");
 }
+
+farToCel(-40);
+
+//Problem 2
